f520f0242dccda2870ac3e49777a00503ff19150
Inagaki <Inagaki@VAIO>
2015-08-08 17:40:00 +0900

    Decorator追加

---
+++ src/Decorator/StringDisplay.java
@@ -0,0 +1,26 @@
+package Decorator;
+
+public class StringDisplay extends Display {
+
+	private String string;
+
+	@Override
+	public int getColumns() {
+		return string.getBytes().length;
+	}
+
+	@Override
+	public int getRows() {
+		return 1;
+	}
+
+	@Override
+	public String getRowText(int row) {
+
+		if (row == 0) {
+			return string;
+		} else {
+			return null;
+		}
+	}
+}
